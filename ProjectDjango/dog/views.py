cb41f3cd65a934a8fff838f19b439a22e231aa3e
niki4cool <niki4cool@gmail.com>
2021-06-05 09:51:35 +0500

    v.3

--- ProjectDjango/dog/views.py
+++ ProjectDjango/dog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
@@ -21,7 +23,8 @@ def index(request):
     })
 
 def post_list(request):
-    return render(request, 'dog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'dog/post_list.html', {'posts': posts})
 
 def turtle(request):
     return render(request, 'dog/turtle.html', {})
